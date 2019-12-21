0e4b428b6cc1cbd44ab3b9e28b03daffbc248c50
Richard Mura <richard@yegobox.com>
2019-12-21 21:59:46 +0200

    now not using logo

--- packages/Webkul/Admin/src/Resources/views/layouts/anonymous-master.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/anonymous-master.blade.php
@@ -76,13 +76,14 @@
 
                 <div class="adjacent-center">
 
-                    <div class="brand-logo">
-                        @if (core()->getConfigData('general.design.admin_logo.logo_image'))
-                            <img src="{{ \Illuminate\Support\Facades\Storage::url(core()->getConfigData('general.design.admin_logo.logo_image')) }}" alt="{{ config('app.name') }}" style="height: 40px; width: 110px;"/>
-                        @else
-                            <img src="{{ asset('vendor/webkul/ui/assets/images/logo.png') }}" alt="{{ config('app.name') }}"/>
-                        @endif
-                    </div>
+{{--                    TODO: un-comment logo once we have our own logo--}}
+{{--                    <div class="brand-logo">--}}
+{{--                        @if (core()->getConfigData('general.design.admin_logo.logo_image'))--}}
+{{--                            <img src="{{ \Illuminate\Support\Facades\Storage::url(core()->getConfigData('general.design.admin_logo.logo_image')) }}" alt="{{ config('app.name') }}" style="height: 40px; width: 110px;"/>--}}
+{{--                        @else--}}
+{{--                            <img src="{{ asset('vendor/webkul/ui/assets/images/logo.png') }}" alt="{{ config('app.name') }}"/>--}}
+{{--                        @endif--}}
+{{--                    </div>--}}
 
                     {!! view_render_event('bagisto.admin.layout.content.before') !!}
 
